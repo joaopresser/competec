232660e6b46082bf49cf903dcf743deff73c9af6
joaopresser <joaoguidpresser@outlook.com>
2025-05-24 10:37:35 -0300

    Update Exercicio2.java

--- aula02/Exercicio2.java
+++ aula02/Exercicio2.java
@@ -1,10 +1,12 @@
 public class Exercicio2 {
   public static void main(String[] args) {
       int x = 0;
+      int somaTotal = 0;
       
       while( x<101 ){
-      System.out.println(x + " ");
       x++;
+      somaTotal += x;
       }
+      System.out.println ("Soma total =" + somaTotal);
   }
 }
